96d90a82362e6c34121c18f3c20c3c006d912a06
Solange Righi <slngvitoria@gmail.com>
2021-03-05 17:41:14 -0300

    Aprendendo DOM

---
+++ 47_DOM/script.js
@@ -0,0 +1,10 @@
+console.log(document);
+
+//mostra tudo o que tem no html (espelho do html)
+
+//--------------------acessando elementos tags-------------------
+
+// console.log(document.body.childNodes); <-- ta dando erro, so consigo acessar diretamente no console
+
+// para acessar mais detalhes do html atraves do dom posso colocar document.childNodes[1] 
+//ou document.childNodes[1].childNodes;
