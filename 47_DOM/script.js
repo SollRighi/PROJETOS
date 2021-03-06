19a19e8825890470d965f0cf40ff2a3711490c93
Solange Righi <slngvitoria@gmail.com>
2021-03-06 20:43:17 -0300

    executando DOM

--- 47_DOM/script.js
+++ 47_DOM/script.js
@@ -1,10 +1,24 @@
-console.log(document);
+// console.log(document);
 
 //mostra tudo o que tem no html (espelho do html)
 
 //--------------------acessando elementos tags-------------------
 
-// console.log(document.body.childNodes); <-- ta dando erro, so consigo acessar diretamente no console
+// console.log(document.body.childNodes); <-- ta dando erro, so consigo acessar 
+//diretamente no console
 
-// para acessar mais detalhes do html atraves do dom posso colocar document.childNodes[1] 
+// para acessar mais detalhes do html atraves do dom posso colocar 
+//document.childNodes[1] 
+
 //ou document.childNodes[1].childNodes;
+
+//------------------Encontrando elementos--------------
+
+// -> getElementsByTagName;
+// -> getElementsbyId;
+// -> getElementsByClassName;
+// -> querySelector;
+
+console.log(document.getElementsbyTagName("h1")); // esta forma nao funciona, 
+//só funciona chamando diretamente no console.
+
